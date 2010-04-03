bbee3d2ff1d64d503e181623ca5c7acbf580d856
Mark Daoust <daoust.mj@gmail.com>
2010-04-03 13:38:01 -0400

    working on square.py

--- square.py
+++ square.py
@@ -6,12 +6,12 @@ from operator import ge
 from helpers import astype
 
 def square(vectors):
-
+    vectors=Matrix(vectors)
+    
     if ge(*vectors.shape):
         cov=vectors.H*vectors
         (val,vec)=numpy.linalg.eigh(vectors.H*vectors)
-        vec=vec.H
-        result=numpy.diagflat(val**(0.5+0j))*vec
+        result=Matrix(val**(0.5+0j)*numpy.array(vec)).H
     else:
         Xcov=vectors*vectors.H
         (Xval,Xvec)=numpy.linalg.eigh(vectors*vectors.H)
@@ -19,13 +19,34 @@ def square(vectors):
         
     return result
 
+def square2(vectors):
+    vectors=Matrix(vectors)
+    
+    if ge(*vectors.shape):
+        cov=vectors.H*vectors
+        (val,vec)=numpy.linalg.eigh(vectors.H*vectors)
+        val=val**(0.5+0j)
+        vec=vec.H
+    else:
+        Xcov=vectors*vectors.H
+        (Xval,Xvec)=numpy.linalg.eigh(vectors*vectors.H)
         
+        val=numpy.diag(Xcov)
+        val=val**(0.5+0j)
+        
+        vec=(Xvec.H*vectors).T
+        vec=Matrix(((val**(-1))*numpy.array(vec)).T)
+        
+    return (val,vec)
+
 if __name__=='__main__':
     for n in xrange(1,20):
         shape=(numpy.random.randint(1,10),numpy.random.randint(1,10),2)
-        vectors=Matrix(astype(numpy.random.rand(*shape),complex))
-
+        vectors=Matrix(astype(numpy.random.randn(*shape),complex))
         
         result=square(vectors);
         assert vectors.H*vectors==result.H*result,"the resulting vectros should have the same covariance as the input"
+        (val,vec)=square2(vectors);
+        val=Matrix(numpy.diagflat(val))
         
+        assert vec.H*(val**2)vec==vectors.H*vectors
