c52d5443d0d76a13e5b796c58e89cad6c47a52bb
Mark Daoust <daoust.mj@gmail.com>
2010-04-10 22:59:29 -0400

    working on replacing 'scale' with 'var' and means to 1 dimensional

--- square.py
+++ square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""
+this module contains one function: squuare
+"""
+
 
 import numpy
 from matrix import Matrix
@@ -10,43 +14,25 @@ def square(vectors):
     
     if ge(*vectors.shape):
         cov=vectors.H*vectors
-        (val,vec)=numpy.linalg.eigh(vectors.H*vectors)
-        result=Matrix(val**(0.5+0j)*numpy.array(vec)).H
-    else:
-        Xcov=vectors*vectors.H
-        (Xval,Xvec)=numpy.linalg.eigh(vectors*vectors.H)
-        result=(Xvec.H*vectors)
-        
-    return result
-
-def square2(vectors):
-    vectors=Matrix(vectors)
-    
-    if ge(*vectors.shape):
-        cov=vectors.H*vectors
-        (val,vec)=numpy.linalg.eigh(vectors.H*vectors)
-        val=val**(0.5+0j)
+        (var,vec)=numpy.linalg.eigh(vectors.H*vectors)
         vec=vec.H
     else:
         Xcov=vectors*vectors.H
         (Xval,Xvec)=numpy.linalg.eigh(vectors*vectors.H)
         
-        val=numpy.diag(Xcov)
-        val=val**(0.5+0j)
+        var=numpy.diag(Xcov)
         
         vec=(Xvec.H*vectors).T
-        vec=Matrix(((val**(-1))*numpy.array(vec)).T)
+        vec=Matrix(((var**(-0.5+0j))*numpy.array(vec)).T)
         
-    return (val,vec)
+    return (var,vec)
 
 if __name__=='__main__':
     for n in xrange(1,20):
         shape=(numpy.random.randint(1,10),numpy.random.randint(1,10),2)
         vectors=Matrix(astype(numpy.random.randn(*shape),complex))
         
-        result=square(vectors);
-        assert vectors.H*vectors==result.H*result,"the resulting vectros should have the same covariance as the input"
-        (val,vec)=square2(vectors);
-        val=Matrix(numpy.diagflat(val))
+        (var,vec)=square(vectors)
+        var=Matrix(numpy.diagflat(var))
         
-        assert vec.H*(val**2)vec==vectors.H*vectors
+        assert vec.H*(var)*vec==vectors.H*vectors
