6bb3c6a38254a3f451b8c8a2d9baf0a56c51ce0c
Mark Daoust <daoust.mj@gmail.com>
2010-12-09 21:30:30 -0500

    more flexible object creator

--- testTools.py
+++ testTools.py
@@ -4,42 +4,50 @@ from mvar import *
 
 pickleName='testObjects.pkl'
 
-def makeObjects(cplx=False,flat=False,ndim=None,seed=None):   
+def makeObjects(cplx=None,flat=None,ndim=None,seed=None):   
     rand=numpy.random.rand
     randn=numpy.random.randn
-    randint=numpy.random.randint
+    randint=lambda x,y: numpy.round((x-0.5)+numpy.random.rand()*(y+0.5))
 
     if seed is not None:
         numpy.random.seed(seed)
 
     if ndim is None:
-        ndim=randint(1,20)
+        ndim=randint(0,20)
 
-    if flat:
-        ndim+=2
-        num=randint(1,ndim-1)
+    if flat is None:
+        num=randint(0,2*ndim)
+    elif flat:
+        num=lambda :randint(0,ndim)
     else:
-        num=2*ndim
+        num=lambda :2*ndim
  
+    if cplx is None:
+        cplx=numpy.rand
+    else:
+        c=bool(cplx)
+        cplx=lambda:c
+
     #create n random vectors, 
     #with a default length of 'ndim', 
     #they can be made complex by setting cplx=True
-    rvec= (lambda n=1:Matrix(helpers.ascomplex(randn(n,ndim,2)))) if cplx else (lambda n=1:Matrix(randn(n,ndim)))
+    rvec= lambda n=1,ndim=ndim:Matrix(helpers.ascomplex(randn(n,ndim,2))) if cplx() else Matrix(randn(n,ndim))
 
     #create random test objects
+    N=num()
     A=Mvar(
         mean=5*randn()*rvec(),
-        vectors=5*randn()*rvec(num),
-        #var=rand(num),
+        vectors=5*randn()*rvec(N),
+        var=rand(N),
     )
 
     B=Mvar.fromCov(
         mean=5*randn()*rvec(),
-        cov=(lambda x:x.H*x)(5*randn()*rvec(num))
+        cov=(lambda x:x.H*x)(5*randn()*rvec(num()))
     )
 
     C=Mvar.fromData(
-        rvec(num+1)
+        rvec(num()+1)
     )
 
     A,B,C=numpy.random.permutation([A,B,C])
@@ -48,8 +56,8 @@ def makeObjects(cplx=False,flat=False,ndim=None,seed=None):
     M2=rvec(ndim)
     E=Matrix.eye(ndim)
     
-    K1=randn()+randn()*(1j if cplx else 0)
-    K2=randn()+randn()*(1j if cplx else 0)
+    K1=randn()+randn()*(1j if cplx() else 0)
+    K2=randn()+randn()*(1j if cplx() else 0)
 
     N=randint(-5,5)
 
