c7ad5841d1e98bc2dd1ad2b6df99f328154af0da
Suki <daoust.mj@gmail.com>
2012-12-25 00:39:37 -0500

    cleanup copyable

--- mvn/decorate/copyable.py
+++ mvn/decorate/copyable.py
@@ -1,18 +1,4 @@
-import copy as cp
-
-def copy(self,other=None,deep=False):
-    """
-    Either return a copy of the object, or copy the other object's dictionary
-    into the self.
-
-    The "deep" key word controls whether it uses copy.copy or copy.deepcopy.
-    """
-     
-    C=cp.deepcopy if deep else cp.copy
-    if other is None:
-        return C(self)
-    else:
-        self.__dict__.update(C(other.__dict__))
+import copy
 
 def copyable(cls):
     '''
@@ -23,6 +9,22 @@ def copyable(cls):
     add a 'copy' method to a class
     '''
     assert not hasattr(cls,'copy')
-    cls.copy=copy
+    cls.copy=Copyable.__dict__['copy']
     return cls
 
+
+
+class Copyable(object):
+    def copy(self,other=None,deep=False):
+        """
+        Either return a copy of the object, or copy the other object's dictionary
+        into the self.
+    
+        The "deep" key word controls whether it uses copy.copy or copy.deepcopy.
+        """
+         
+        C=copy.deepcopy if deep else copy.copy
+        if other is None:
+            return C(self)
+        else:
+            self.__dict__.update(C(other.__dict__))
