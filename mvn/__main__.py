b40b47aec58f504faf4b1dfc7d203c4674df24d6
Suki <daoust.mj@gmail.com>
2012-12-26 06:54:06 -0500

    setup for "python -m mvn" to run tests

--- mvn/__main__.py
+++ mvn/__main__.py
@@ -1,11 +1,14 @@
-from mvn import Mvn
+import os
+import sys
 
-L1=Mvn(mean=[0,0],vectors=[[1,1],[1,-1]], var=[numpy.inf,0.5])
-L2=Mvn(mean=[1,0],vectors=[0,1],var=numpy.inf) 
+import test
 
-L1 & L2
+#import pychecker.checker? as check
+#import pyflakes?
+#import pylint?
+#import sphinx
 
-L1.given(dims=0,value=1) == L1&L2 
-(L1&L2).mean==[1,1] 
-(L1&L2).cov==[[0,0],[0,2]] 
-    
+test.main(sys.argv[1:])
+
+
+
